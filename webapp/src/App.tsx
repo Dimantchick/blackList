eeab4a2eddae162ed16962536410a08f76f3b11d
Решетинский Дмитрий <dmitrii.reshetinskii@r-x.team>
2025-05-11 20:28:09 +0700

    отключил телеметрию

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,26 +1,24 @@
-import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from "react-admin";
-import {
-  amplicodeDarkTheme,
-  amplicodeLightTheme,
-} from "./themes/amplicodeTheme/amplicodeTheme";
-import { dataProvider } from "./dataProvider";
-import { NetworksList } from "./networks/NetworksList";
-import { NetworksCreate } from "./networks/NetworksCreate";
-import { NetworksEdit } from "./networks/NetworksEdit";
+import {Admin, Resource} from "react-admin";
+import {amplicodeDarkTheme, amplicodeLightTheme,} from "./themes/amplicodeTheme/amplicodeTheme";
+import {dataProvider} from "./dataProvider";
+import {NetworksList} from "./networks/NetworksList";
+import {NetworksCreate} from "./networks/NetworksCreate";
+import {NetworksEdit} from "./networks/NetworksEdit";
 import authProvider from "./formLoginAuthProvider";
-import { i18nProvider } from "./i18nProvider";
-import { UsersList } from "./users/UsersList";
-import { UsersEdit } from "./users/UsersEdit";
-import { UsersCreate } from "./users/UsersCreate";
+import {i18nProvider} from "./i18nProvider";
+import {UsersList} from "./users/UsersList";
+import {UsersEdit} from "./users/UsersEdit";
+import {UsersCreate} from "./users/UsersCreate";
 
 export const App = () => {
   return (
     <Admin
-authProvider={authProvider}
+      authProvider={authProvider}
       dataProvider={dataProvider}
       lightTheme={amplicodeLightTheme}
       darkTheme={amplicodeDarkTheme} requireAuth={true}
       i18nProvider={i18nProvider}
+      disableTelemetry={true}
     >
     
 <Resource
