9ddd1faea91dd50e408f82b57ca25824e3fe7150
Решетинский Дмитрий <dmitrii.reshetinskii@r-x.team>
2025-05-18 12:56:32 +0700

    Не работала фильтрация доменов

--- webapp/src/domains/DomainsList.tsx
+++ webapp/src/domains/DomainsList.tsx
@@ -1,43 +1,34 @@
-import { Box, Typography } from '@mui/material';
-import { 
-    Datagrid, 
-    List, 
-    NumberField, 
-    TextField, 
+import {Box, Typography} from '@mui/material';
+import {
+    Datagrid,
+    List,
+    NumberField,
+    TextField,
     TextInput,
-    TopToolbar,
-    CreateButton,
 } from 'react-admin';
-
-const ListActions = () => (
-    <TopToolbar>
-        <CreateButton />
-    </TopToolbar>
-);
 
 export const DomainsList = () => {
     return (
-        <List 
+        <List
             resource="domains"
             filters={[
-                <TextInput source="domainContains" label="Поиск по домену" />, 
-                <TextInput source="commentContains" label="Поиск по комментарию" />
+                <TextInput source="domainContains" label="Поиск по домену"/>,
+                <TextInput source="commentContains" label="Поиск по комментарию"/>
             ]}
-            actions={<ListActions />}
         >
-            <Box sx={{ 
-                padding: '16px', 
-                backgroundColor: 'transparent' 
+            <Box sx={{
+                padding: '16px',
+                backgroundColor: 'transparent'
             }}>
                 <Typography variant="body2">
                     Домены, по которым будет регулярно производиться проверка и адреса будут добавлены в маршруты.
                 </Typography>
             </Box>
             <Datagrid rowClick="edit">
-                <NumberField source="id" />
-                <TextField source="domain" />
-                <TextField source="comment" 
-                sx={{ wordBreak: 'break-word!important', whiteSpace: 'pre-wrap!important' }}
+                <NumberField source="id"/>
+                <TextField source="domain"/>
+                <TextField source="comment"
+                           sx={{wordBreak: 'break-word!important', whiteSpace: 'pre-wrap!important'}}
                 />
             </Datagrid>
         </List>
